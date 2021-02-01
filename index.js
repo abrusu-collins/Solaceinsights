06a9413ca3b601db1b6e8cac53915b0df621e2a0
abrusu-collins <snillochemingson@gmail.com>
2021-01-31 16:31:04 -0800

    Left with posts

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-
-const toggle = document.getElementById('toggle');
-
-const nav = document.getElementById('nav');
-
-toggle.addEventListener('click', () => 
-  nav.classList.toggle('active'));
-
+document.querySelector('.toggler').onclick = () => {
+  document.querySelector('.nav').classList.toggle('open');
+}
